0e2671199afe63d6ff765afcf538aa4c1a47dc88
Itamar Denkberg <ItamarDenkberg@users.noreply.github.com>
2021-07-12 17:15:10 +0300

    Fixes

--- src/main/java/io/github/itamardenkberg/allyoucaneat/common/blocks/EmptyWineBottleBlock.java
+++ src/main/java/io/github/itamardenkberg/allyoucaneat/common/blocks/EmptyWineBottleBlock.java
@@ -6,9 +6,16 @@ import io.github.itamardenkberg.allyoucaneat.core.init.StatInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.Property;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
@@ -17,17 +24,35 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
 public class EmptyWineBottleBlock extends Block {
 	private static final VoxelShape SHAPE = Block.makeCuboidShape(6, 0, 6, 10, 16, 10);
+	public static final BooleanProperty WATERLOGGED;
 
 	public EmptyWineBottleBlock(Properties properties) {
 		super(properties);
+		this.setDefaultState((BlockState) ((BlockState) this.stateContainer.getBaseState()).with(WATERLOGGED, false));
 	}
 
 	public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context) {
 		return SHAPE;
+	}
+
+	@SuppressWarnings("deprecation")
+	public BlockState updatePostPlacement(BlockState state, Direction direction, BlockState blockState, IWorld world,
+			BlockPos pos, BlockPos blockPos) {
+		if ((Boolean) state.get(WATERLOGGED)) {
+			world.getPendingFluidTicks().scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+
+		return super.updatePostPlacement(state, direction, blockState, world, pos, blockPos);
+	}
+
+	@SuppressWarnings("deprecation")
+	public FluidState getFluidState(BlockState state) {
+		return (Boolean) state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
 	}
 
 	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player,
@@ -59,5 +84,13 @@ public class EmptyWineBottleBlock extends Block {
 				return ActionResultType.PASS;
 			}
 		}
+	}
+
+	protected void fillStateContainer(Builder<Block, BlockState> builder) {
+		builder.add(new Property[] { WATERLOGGED });
+	}
+
+	static {
+		WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	}
 }
--- src/main/java/io/github/itamardenkberg/allyoucaneat/common/blocks/WineBottleBlock.java
+++ src/main/java/io/github/itamardenkberg/allyoucaneat/common/blocks/WineBottleBlock.java
@@ -5,15 +5,20 @@ import io.github.itamardenkberg.allyoucaneat.core.init.ItemInit;
 import io.github.itamardenkberg.allyoucaneat.core.init.StatInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.Property;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
@@ -23,16 +28,19 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
-public class WineBottleBlock extends Block {
+public class WineBottleBlock extends Block implements IWaterLoggable {
 	public static final IntegerProperty LEVEL = BlockStateProperties.LEVEL_0_3;
+	public static final BooleanProperty WATERLOGGED;
 	private static final VoxelShape SHAPE = Block.makeCuboidShape(6, 0, 6, 10, 16, 10);
 	private static final VoxelShape INSIDE = makeCuboidShape(6.1, 0, 6.1, 9.899999999999999, 11, 9.9);
 
 	public WineBottleBlock(Properties properties) {
 		super(properties);
-		this.setDefaultState((BlockState) ((BlockState) this.stateContainer.getBaseState()).with(LEVEL, 3));
+		this.setDefaultState(
+				(BlockState) ((BlockState) this.stateContainer.getBaseState()).with(LEVEL, 3).with(WATERLOGGED, false));
 	}
 
 	public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context) {
@@ -41,6 +49,21 @@ public class WineBottleBlock extends Block {
 
 	public VoxelShape getRaytraceShape(BlockState state, IBlockReader reader, BlockPos pos) {
 		return INSIDE;
+	}
+
+	@SuppressWarnings("deprecation")
+	public BlockState updatePostPlacement(BlockState state, Direction direction, BlockState blockState, IWorld world,
+			BlockPos pos, BlockPos blockPos) {
+		if ((Boolean) state.get(WATERLOGGED)) {
+			world.getPendingFluidTicks().scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+
+		return super.updatePostPlacement(state, direction, blockState, world, pos, blockPos);
+	}
+
+	@SuppressWarnings("deprecation")
+	public FluidState getFluidState(BlockState state) {
+		return (Boolean) state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
 	}
 
 	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player,
@@ -129,6 +152,10 @@ public class WineBottleBlock extends Block {
 	}
 
 	protected void fillStateContainer(Builder<Block, BlockState> builder) {
-		builder.add(new Property[] { LEVEL });
+		builder.add(new Property[] { LEVEL, WATERLOGGED });
+	}
+	
+	static {
+		WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	}
 }
